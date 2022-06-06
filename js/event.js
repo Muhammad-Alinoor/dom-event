008cf08a706a55043462e95ff2314ae696f1f483
Muhammad-Alinoor <alinoorm00@gmail.com>
2022-06-06 23:31:06 +0600

    3rd way to set onclick event

--- js/event.js
+++ js/event.js
@@ -1,3 +1,8 @@
 function redMe(){
     document.body.style.backgroundColor = 'red';
 }
+const blueMe = document.getElementById('blue-me');
+blueMe.onclick = makeBlue;
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
